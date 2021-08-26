9770e9fbf2c368ffdc78c82e03027506079c742c
01a4 <87416787+01a4@users.noreply.github.com>
2021-08-26 16:07:12 +0900

    Add files via upload

--- my_service.cpp
+++ my_service.cpp
@@ -4,28 +4,98 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <move_base_msgs/MoveBaseActionResult.h>
 
-/*void cbCheckArrivalStatusTB3P(const move_base_msgs::MoveBaseActionResult rcvMoveBaseActionResult)
-  {
-    if (rcvMoveBaseActionResult.status.status == 3)
+
+class Myservice
+{
+public:
+	void cbCheckArrivalStatus_0(const move_base_msgs::MoveBaseActionResult rcvMoveBaseActionResult){
+	if (rcvMoveBaseActionResult.status.status == 3)
     {
-      state = true;
-      
+      state[0] = true;
+      ROS_INFO("change state[0]");
     }
     else
     {
-      ROS_INFO("cbCheckArrivalStatusTB3P : %d", rcvMoveBaseActionResult.status.status);
+      ROS_INFO("cbCheckArrivalStatus_0 : %d", rcvMoveBaseActionResult.status.status);
     }
   }
 
-*/
-int main(int argc, char **argv)
+
+	void cbCheckArrivalStatus_1(const move_base_msgs::MoveBaseActionResult rcvMoveBaseActionResult){
+        if (rcvMoveBaseActionResult.status.status == 3)
+    {
+      state[1] = true;
+      ROS_INFO("change state[1]");
+    }
+    else
+    {
+      ROS_INFO("cbCheckArrivalStatus_1 : %d", rcvMoveBaseActionResult.status.status);
+    }
+  }
+
+
+void fnPubPose()
 {
-ros::init(argc, argv, "my_service");
+if(state[0])
+{
+pubPoseStampedTb3p.publish(poseStampedTable[0]);
+ROS_INFO("state[0] publish");
+state[0] = false;
+}
+else if(state[1])
+{
+pubPoseStampedTb3p.publish(poseStampedTable[1]);
+ROS_INFO("state[1] publish");
+state[1] = false;
+
+}
+}
+
+
+void fnInitParam()
+{
+poseStampedTable[0].header.seq = 0;
+poseStampedTable[0].header.stamp.sec = 0;
+poseStampedTable[0].header.stamp.nsec = 0;
+poseStampedTable[0].header.frame_id = "map";
+
+poseStampedTable[0].pose.position.x = point[0][0];
+poseStampedTable[0].pose.position.y = point[0][1];
+poseStampedTable[0].pose.position.z = 0;
+
+poseStampedTable[0].pose.orientation.x = 0;
+poseStampedTable[0].pose.orientation.y = 0;
+poseStampedTable[0].pose.orientation.z = 1;
+poseStampedTable[0].pose.orientation.w = -0.5;
+poseStampedTable[1].header.seq = 0;
+poseStampedTable[1].header.stamp.sec = 0;
+poseStampedTable[1].header.stamp.nsec = 0;
+poseStampedTable[1].header.frame_id = "map";
+
+poseStampedTable[1].pose.position.x = point[1][0];
+poseStampedTable[1].pose.position.y = point[1][1];
+poseStampedTable[1].pose.position.z = 0;
+
+poseStampedTable[1].pose.orientation.x = 0;
+poseStampedTable[1].pose.orientation.y = 0;
+poseStampedTable[1].pose.orientation.z = 1;
+poseStampedTable[1].pose.orientation.w = -0.5;
+
+
+
+}
+
+
+
+
+	Myservice(){
+
+	fnInitParam();
 ros::NodeHandle nh_;
-geometry_msgs::PoseStamped poseStampedTable;
-ros::Rate loop_rate(5);
+//geometry_msgs::PoseStamped poseStampedTable[2];
+//ros::Rate loop_rate(5);
 
-
+/*
 double point[2][2] = {
 
 	{-1.6285, -0.4183,},
@@ -33,61 +103,120 @@ double point[2][2] = {
 	{-2.1424, 1.1759}
 };
 
-bool state = true;
-void cbCheckArrivalStatusTB3P(const move_base_msgs::MoveBaseActionResult rcvMoveBaseActionResult)
-  {
-    if (rcvMoveBaseActionResult.status.status == 3)
-    {
-      state = true;
 
-    }
-    else
-    {
-      ROS_INFO("cbCheckArrivalStatusTB3P : %d", rcvMoveBaseActionResult.status.status);
-    }
-  }
 
-poseStampedTable.header.seq = 0;
-poseStampedTable.header.stamp.sec = 0;
-poseStampedTable.header.stamp.nsec = 0;
-poseStampedTable.header.frame_id = "map";
+poseStampedTable[0].header.seq = 0;
+poseStampedTable[0].header.stamp.sec = 0;
+poseStampedTable[0].header.stamp.nsec = 0;
+poseStampedTable[0].header.frame_id = "map";
 
-poseStampedTable.pose.position.x = point[0][0];
-poseStampedTable.pose.position.y = point[0][1];
-poseStampedTable.pose.position.z = 0;
+poseStampedTable[0].pose.position.x = point[0][0];
+poseStampedTable[0].pose.position.y = point[0][1];
+poseStampedTable[0].pose.position.z = 0;
 
-poseStampedTable.pose.orientation.x = 0;
-poseStampedTable.pose.orientation.y = 0;
-poseStampedTable.pose.orientation.z = 1;
-poseStampedTable.pose.orientation.w = -0.5;
+poseStampedTable[0].pose.orientation.x = 0;
+poseStampedTable[0].pose.orientation.y = 0;
+poseStampedTable[0].pose.orientation.z = 1;
+poseStampedTable[0].pose.orientation.w = -0.5;
 
+
+poseStampedTable[1].header.seq = 0;
+poseStampedTable[1].header.stamp.sec = 0;
+poseStampedTable[1].header.stamp.nsec = 0;
+poseStampedTable[1].header.frame_id = "map";
+
+poseStampedTable[1].pose.position.x = point[1][0];
+poseStampedTable[1].pose.position.y = point[1][1];
+poseStampedTable[1].pose.position.z = 0;
+
+poseStampedTable[1].pose.orientation.x = 0;
+poseStampedTable[1].pose.orientation.y = 0;
+poseStampedTable[1].pose.orientation.z = 1;
+poseStampedTable[1].pose.orientation.w = -0.5;
+
+*/
 //ros::Publisher pubServiceStatusPadtb3p;
 
-ros::Subscriber sub_arrival_status_tb3p;
-
+/*ros::Subscriber sub_arrival_status_0;
+ros::Subscriber sub_arrival_status_1;
 ros::Publisher pubPoseStampedTb3p;
+*/
+
 pubPoseStampedTb3p = nh_.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1);
 
 
-sub_arrival_status_tb3p = nh_.subscribe("/move_base/result", 1, &cbCheckArrivalStatusTB3P, this);
+sub_arrival_status_0 = nh_.subscribe("/move_base/result", 1, &Myservice::cbCheckArrivalStatus_0, this);
+sub_arrival_status_1 = nh_.subscribe("/move_base/result", 1, &Myservice::cbCheckArrivalStatus_1, this);
+ros::Rate loop_rate(5);
+
 
 while (ros::ok())
 {
-if(state == true)
-{
-	pubPoseStampedTb3p.publish(poseStampedTable);
-	ROS_INFO("point publish done");
-	state = false;
-}
+
+
+fnPubPose();
+
 
 
 ros::spinOnce();
 loop_rate.sleep();
 }
+	}
+	
+private:
+	double point[2][2] = {
 
+        {-1.6285, -0.4183,},
+
+        {-2.1424, 1.1759}
+};
+
+geometry_msgs::PoseStamped poseStampedTable[2];
+
+ros::Subscriber sub_arrival_status_0;
+ros::Subscriber sub_arrival_status_1;
+ros::Publisher pubPoseStampedTb3p;
+
+/*poseStampedTable[0].header.seq = 0;
+poseStampedTable[0].header.stamp.sec = 0;
+poseStampedTable[0].header.stamp.nsec = 0;
+poseStampedTable[0].header.frame_id = "map";
+
+poseStampedTable[0].pose.position.x = point[0][0];
+poseStampedTable[0].pose.position.y = point[0][1];
+poseStampedTable[0].pose.position.z = 0;
+
+poseStampedTable[0].pose.orientation.x = 0;
+poseStampedTable[0].pose.orientation.y = 0;
+poseStampedTable[0].pose.orientation.z = 1;
+poseStampedTable[0].pose.orientation.w = -0.5;
+poseStampedTable[1].header.seq = 0;
+poseStampedTable[1].header.stamp.sec = 0;
+poseStampedTable[1].header.stamp.nsec = 0;
+poseStampedTable[1].header.frame_id = "map";
+
+poseStampedTable[1].pose.position.x = point[1][0];
+poseStampedTable[1].pose.position.y = point[1][1];
+poseStampedTable[1].pose.position.z = 0;
+
+poseStampedTable[1].pose.orientation.x = 0;
+poseStampedTable[1].pose.orientation.y = 0;
+poseStampedTable[1].pose.orientation.z = 1;
+poseStampedTable[1].pose.orientation.w = -0.5;
+
+*/
+bool state[2] = {true, true};
+};
+
+int main(int argc, char **argv)
+{
+
+ros::init(argc, argv, "my_service");
+Myservice myservice;
 ros::spin();
 return 0;
 }
+
 
 
 
